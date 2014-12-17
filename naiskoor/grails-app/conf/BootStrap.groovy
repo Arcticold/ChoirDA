56693032123592b7f009c0d10821d93ffe85b489
mlaidma <martinlaidma@gmail.com>
2014-12-17 16:33:31 +0200

    And another. Someone should do something about issue #21

--- naiskoor/grails-app/conf/BootStrap.groovy
+++ naiskoor/grails-app/conf/BootStrap.groovy
@@ -9,13 +9,10 @@ class BootStrap {
     	def devRole = Role.findOrSaveWhere(authority: 'ROLE_DEVELOPER')
     	def adminRole = Role.findOrSaveWhere(authority: 'ROLE_ADMIN')
     	def user = User.findOrSaveWhere(username: 'dev', password: 'dev', enabled: true, accountExpired: false, accountLocked: false, passwordExpired: false, firstName: 'developer', lastName: 'developer', roleInChoir: 'Bass', eMail: 'dev@naiskoor.ee')
-        user.save(flush: true)
     	def adminUser = User.findOrSaveWhere(username: 'admin', password: 'admin', enabled: true, accountExpired: false, accountLocked: false, passwordExpired: false,  firstName: 'admin', lastName: 'admin', roleInChoir: 'Soprano', eMail: 'admin@naiskoor.ee')
-        adminUser.save(flush: true)
     	
     	if (!user.authorities.contains('ROLE_DEVELOPER')) {
     		UserRole.create(user, devRole, true)
-
     	}
 
     	if (!adminUser.authorities.contains('ROLE_ADMIN')) {
